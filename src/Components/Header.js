c4e6e20fe4a57562b7cda7113f451c5ddc6ca4c0
LeCray <mistercryptogrid@gmail.com>
2018-06-18 00:04:29 +0200

    Added a navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,66 @@
-import React, { Component}  from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  NavDropdown,
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-  Button
-} from 'react-bootstrap';
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+            dropdownOpen: false
+        };
+    this.onClick = this.onClick.bind(this);
+    this.toggle = this.toggle.bind(this);
+    }
 
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
 
-const Header = () => (
- <Navbar>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#home">React-Bootstrapper</a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav>
-    <NavItem eventKey={1} href="#">
-      Link
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Link
-    </NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-  
-    </NavDropdown>
-  </Nav>
-</Navbar>
-)
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
 
-export default Header
+    render() {
+        return (
+            <Router>
+                <Navbar color="indigo" dark expand="md" scrolling>
+                    <NavbarBrand href="/">
+                        <strong>Navbar</strong>
+                    </NavbarBrand>
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem active>
+                              <NavLink to="#">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Features</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Pricing</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                              <DropdownToggle nav caret>Dropdown</DropdownToggle>
+                              <DropdownMenu>
+                                  <DropdownItem href="#">Action</DropdownItem>
+                                  <DropdownItem href="#">Another Action</DropdownItem>
+                                  <DropdownItem href="#">Something else here</DropdownItem>
+                                  <DropdownItem href="#">Something else here</DropdownItem>
+                              </DropdownMenu>
+                              </Dropdown>
+                          </NavItem>
+                        </NavbarNav>
+                        
+                    </Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
+
 
 
 render((
