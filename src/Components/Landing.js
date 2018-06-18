d657af877ea23bc43f7908ca48cea5aeb267acba
LeCray <mistercryptogrid@gmail.com>
2018-06-18 14:50:08 +0200

    Making progress

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -25,10 +25,18 @@ export default class Landing extends Component {
 
 	render() {		
 		return(  
-			<div >				
+			<div>				
         <img src={require("../parkupp_gif.png")} style={{width: "100%"}} /> 
+        <div style={{backgroundColor: "#2bbbad", textAlign: "center"}}>
 
-				<Button bsStyle="success">Success</Button>
+          <div style={{paddingTop: 200, paddingBottom: 200, flex: 1}}>
+            <h1 style={{color: "white", fontSize: 100}}><strong>ParkUpp</strong></h1><br/>
+            <h2 style={{color: "white", fontSize: 30}}><strong>Reimagine. Parking.</strong></h2>
+          </div>
+
+        </div>
+
+				
         
 			</div>
 		)
