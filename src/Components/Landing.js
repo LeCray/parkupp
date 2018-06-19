636c71b21dc47fb309dd391f6c4ef2fa3224aa83
LeCray <mistercryptogrid@gmail.com>
2018-06-19 02:39:44 +0200

    nearly there

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Input, Button, Fa, Card, CardBody, ModalFooter } from 'mdbreact';
 import Background from '../parkupp_gif.png';
 
 
@@ -16,16 +16,32 @@ import {
   Row,
   Col,
   Jumbotron,
-  Button
+  //Button
 } from 'react-bootstrap';
 
 
 
 export default class Landing extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
 
 	render() {		
 		return(  
-			<div>				
+			<div style={{overflow: "hidden"}}>				
                 <img src={require("../parkupp_gif.png")} style={{width: "100%"}} /> 
                 <div style={{textAlign: "center"}}>
 
@@ -153,25 +169,64 @@ export default class Landing extends Component {
 
                     {/* ABOUT US*/} 
                     <div style={{paddingTop: 50, paddingBottom: 50, backgroundColor: "#121a5e", color: "white"}}>
-                        <h1 style={{fontSize: 50}}>About Us</h1>
+                        <div class="container" style={{padding: 20}}>
+                            <h1 style={{fontSize: 50}}>About Us</h1>
 
-                        <p style={{marginTop: 70, fontSize: 20}}>
-                            At the ParkUpp family, we wake up everyday to solve some of the land<br/>
-                            allocation issues that the world is facing. <br/>
-                            <br/>
-                            The first problem we are solving is the one of parking. When people<br/>
-                            buy cars, they don’t realise that the car they are buying will be parked<br/>
-                            95% of the time. Furthermore, we don’t believe that new parkades<br/>
-                            need to be built in order to accommodate new cars that are being<br/>
-                            bought everyday. There are enough driveways in the world that are<br/>
-                            already solving this problem but are currently not being used to their<br/>
-                            fullest capacity. Throughout the world, only 4% of land is allocated to<br/>
-                            parking therefore making the resource very scarce.<br/>
-                            <br/>
-                            ParkUpp is a peer-to-peer platform that will connect people with idle<br/>
-                            driveways to the ones who desperately need parking. Let’s share<br/>
-                            parking and use the land for something more useful.
-                        </p>
+                            <p style={{marginTop: 70, fontSize: 20}}>
+                                At the ParkUpp family, we wake up everyday to solve some of the land<br/>
+                                allocation issues that the world is facing. <br/>
+                                <br/>
+                                The first problem we are solving is the one of parking. When people<br/>
+                                buy cars, they don’t realise that the car they are buying will be parked<br/>
+                                95% of the time. Furthermore, we don’t believe that new parkades<br/>
+                                need to be built in order to accommodate new cars that are being<br/>
+                                bought everyday. There are enough driveways in the world that are<br/>
+                                already solving this problem but are currently not being used to their<br/>
+                                fullest capacity. Throughout the world, only 4% of land is allocated to<br/>
+                                parking therefore making the resource very scarce.<br/>
+                                <br/>
+                                ParkUpp is a peer-to-peer platform that will connect people with idle<br/>
+                                driveways to the ones who desperately need parking. Let’s share<br/>
+                                parking and use the land for something more useful.
+                            </p>
+                               
+                            <hr style={{borderColor: "white", width: 120, marginTop: 100}}/> 
+                        </div>                       
+                    </div>
+
+                    <div style={{paddingTop: 50, paddingBottom: 50, backgroundColor: "#2bbbad", color: "white"}}>
+                        <div class="container">
+                            <h1 style={{fontSize: 50}}>Sign Up to ParkUpp</h1>
+
+                        
+                            
+                            
+                                <section className="form-dark">
+                                  <div class="row">
+                                    <div class="col-sm-4 offset-sm-4">
+                                      
+                                        <div className="text-white">                                      
+                                          <Input label="Name" group type="text" validate />
+                                          <Input label="Email Address" group type="email" validate/>
+                                       
+
+                                          <Row className="d-flex align-items-center mb-4">
+                                            <div className="text-center mb-3 col-md-12">
+                                              <Button color="default" rounded type="button" className="btn-block z-depth-1">Sign Up</Button>
+                                            </div>
+                                          </Row>
+                                      
+                                        </div>
+                                      
+                                    </div>
+                                  </div>
+                                </section>
+                              
+                        </div>
+                            
+                        
+
+                        
                            
                         <hr style={{borderColor: "white", width: 120, marginTop: 100}}/>                        
                     </div>
