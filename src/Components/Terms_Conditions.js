38f4e9059738eae0a8fb7b152bddfa2e69059fb5
LeCray <mistercryptogrid@gmail.com>
2018-06-21 02:12:50 +0200

    Implementing Ts and Cs

--- src/Components/Terms_Conditions.js
+++ src/Components/Terms_Conditions.js
@@ -22,10 +22,35 @@ export default class Terms_Conditions extends Component {
 
 	render() {		
 		return(  
-			<div class="container">
-				<h1>TERMS AND CONDITIONS PAGE</h1>
+			<div style={{backgroundColor: "#2bbbad", border: "1px solid red", height: "100vh", 
+                        color: "white", paddingTop: 100, textAlign: "center"}}>
 
-				<Button bsStyle="success">Success</Button>
+                <div style={{textAlign: "center"}}>
+                    <h1>Ts & Cs</h1>
+                    <p>Long and boring but important stuff</p>
+                </div>
+                <div style={{marginTop: 50, backgroundColor: "white", height: "50%", color: "black", padding: 50, overflow: "scroll", border: "1px solid red"}}>
+                
+                    Terms & Conditions
+                
+                
+Welcome to www.parkupp.co.za (“website”) and/or the ParkUpp mobile application (“App”). By using this website/App and the services offered by Lesa & Co (Pty) Limited (registration number 2015/280619/07) (together with its parents, subsidiaries, affiliates, agents, representatives, consultants, employees, officers, and directors — collectively, “ParkUpp,”“we,” or “us”), you are agreeing to these legally binding Terms and Conditions (including the Privacy Policy) (collectively the “Terms”). Please read these Terms carefully and should you have enquiries, please contact us at info@parkupp.co.za.
+There may be legal notices on other areas of this website/App which relate to your use of the website/App and/or the services offered by ParkUpp, all of which will, together with these Terms, as may be amended from time to time by ParkUpp, govern your use of this website/App as well as the Services.
+________________________________________
+About ParkUpp
+ParkUpp is a mobile application that helps users find parking at certain shopping centres, retail parks, corporate office parks and other parking facilities (“Centres”) and pay for their parking tickets on their mobile app. The ParkUpp services include ticketless entry and exit to and from Centres, special offers (including vouchers and coupons) from retailers, brands or partners (or partner programs) in each Centre and free, discounted or extended parking (subject to conditions) (collectively the “Services”).
+________________________________________
+Acceptance
+These Terms apply to your use of the website/App. You must read these Terms carefully, and we recommend that you print and keep a copy for your future reference. By accessing, browsing, using or registering for our Services on the website/App, you confirm that you have read, understood and agree to these Terms in their entirety. If you do not agree to all of these Terms, you must not use this website/App or any Services related thereto.
+________________________________________
+Website/App Registration
+As an unregistered user you will be able to browse the Services offered on the website/App. However, when you choose to make use of the services offered on this website/App, you will be required to login with your username and password. If you do not have a username and password you will be required to register on the website or your mobile device. Registration requires filling in a short form which will create login details. Once the registration is complete and you are logged in you will then be able to make use of the Services.
+The information you provide must be accurate and complete. You may not impersonate anyone else, choose names that are offensive or that violate anyone’s rights. If you don’t follow these rules, we may cancel your account. Every person must register their own account and you are not entitled to permit anyone other than you to use your account. Charges that are incurred in relation to the Services must be paid by way of credit card or any other payment means as offered during the registration process. Your credit card details will be required. Purchases made via credit card will be verified, authenticated and stored by PayGate. PayGate is a certified Payment System Operator (PASA) & PCI DSS Level 1 Service Provider, ensuring payment data security utilising PCI compliant processes and technology. By registering an account or downloading the App, you expressly consent to ParkUpp collecting the personal information you provide, and to ParkUpp processing your personal information as required to provide the Services. This will be done in compliance with ParkUpp’s Privacy Policy. By registering on the website/App, you also expressly consent to receive direct marketing from ParkUpp about goods or services offered by ParkUpp and any other affiliate, or entity within the ParkUpp group of companies.
+
+
+                </div>
+
+				
 			</div>
 		)
 	}
