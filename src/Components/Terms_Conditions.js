58b42063af679d55e7adb91dc0086cf13b5a667d
LeCray <mistercryptogrid@gmail.com>
2018-06-21 12:15:25 +0200

    Putting in the ts and cs

--- src/Components/Terms_Conditions.js
+++ src/Components/Terms_Conditions.js
@@ -34,21 +34,99 @@ export default class Terms_Conditions extends Component {
                 <div style={{backgroundColor: "white", height: "70%", width: "70%", color: "black",
                              padding: 50, overflow: "scroll", alignSelf: "center", margin: "0 auto"}}>
                 
-                    Terms & Conditions
+                
+                    <h2>Terms & Conditions</h2>
                 
                 
-Welcome to www.parkupp.co.za (“website”) and/or the ParkUpp mobile application (“App”). By using this website/App and the services offered by Lesa & Co (Pty) Limited (registration number 2015/280619/07) (together with its parents, subsidiaries, affiliates, agents, representatives, consultants, employees, officers, and directors — collectively, “ParkUpp,”“we,” or “us”), you are agreeing to these legally binding Terms and Conditions (including the Privacy Policy) (collectively the “Terms”). Please read these Terms carefully and should you have enquiries, please contact us at info@parkupp.co.za.
-There may be legal notices on other areas of this website/App which relate to your use of the website/App and/or the services offered by ParkUpp, all of which will, together with these Terms, as may be amended from time to time by ParkUpp, govern your use of this website/App as well as the Services.
-________________________________________
-About ParkUpp
-ParkUpp is a mobile application that helps users find parking at certain shopping centres, retail parks, corporate office parks and other parking facilities (“Centres”) and pay for their parking tickets on their mobile app. The ParkUpp services include ticketless entry and exit to and from Centres, special offers (including vouchers and coupons) from retailers, brands or partners (or partner programs) in each Centre and free, discounted or extended parking (subject to conditions) (collectively the “Services”).
-________________________________________
-Acceptance
-These Terms apply to your use of the website/App. You must read these Terms carefully, and we recommend that you print and keep a copy for your future reference. By accessing, browsing, using or registering for our Services on the website/App, you confirm that you have read, understood and agree to these Terms in their entirety. If you do not agree to all of these Terms, you must not use this website/App or any Services related thereto.
-________________________________________
-Website/App Registration
-As an unregistered user you will be able to browse the Services offered on the website/App. However, when you choose to make use of the services offered on this website/App, you will be required to login with your username and password. If you do not have a username and password you will be required to register on the website or your mobile device. Registration requires filling in a short form which will create login details. Once the registration is complete and you are logged in you will then be able to make use of the Services.
-The information you provide must be accurate and complete. You may not impersonate anyone else, choose names that are offensive or that violate anyone’s rights. If you don’t follow these rules, we may cancel your account. Every person must register their own account and you are not entitled to permit anyone other than you to use your account. Charges that are incurred in relation to the Services must be paid by way of credit card or any other payment means as offered during the registration process. Your credit card details will be required. Purchases made via credit card will be verified, authenticated and stored by PayGate. PayGate is a certified Payment System Operator (PASA) & PCI DSS Level 1 Service Provider, ensuring payment data security utilising PCI compliant processes and technology. By registering an account or downloading the App, you expressly consent to ParkUpp collecting the personal information you provide, and to ParkUpp processing your personal information as required to provide the Services. This will be done in compliance with ParkUpp’s Privacy Policy. By registering on the website/App, you also expressly consent to receive direct marketing from ParkUpp about goods or services offered by ParkUpp and any other affiliate, or entity within the ParkUpp group of companies.
+                    Welcome to www.parkupp.co.za (“website”) and/or the ParkUpp mobile application (“App”). 
+                    By using this website/App and the services offered by Lesa & Co (Pty) Limited 
+                    (registration number 2015/280619/07) (together with its parents, subsidiaries, affiliates, agents, 
+                    representatives, consultants, employees, officers, and directors — collectively, “ParkUpp,”“we,” or “us”), 
+                    you are agreeing to these legally binding Terms and Conditions (including the Privacy Policy) (collectively the “Terms”). 
+                    Please read these Terms carefully and should you have enquiries, please contact us at info@parkupp.co.za.
+
+                    <br/><br/>
+                    There may be legal notices on other areas of this website/App which relate to your use of the website/App and/or the services offered by ParkUpp, 
+                    all of which will, together with these Terms, as may be amended from time to time by ParkUpp, govern your use of this website/App as 
+                    well as the Services.
+
+                    <br/><br/>
+
+                    <h4>About ParkUpp</h4>
+                    ParkUpp is a mobile application that helps users find parking at certain shopping centres, retail parks, corporate office parks and 
+                    other parking facilities (“Centres”) and pay for their parking tickets on their mobile app. The ParkUpp services include ticketless entry and 
+                    exit to and from Centres, special offers (including vouchers and coupons) from retailers, brands or partners (or partner programs) 
+                    in each Centre and free, discounted or extended parking (subject to conditions) (collectively the “Services”).
+                    
+                    <br/><br/>
+
+                    <h4>Acceptance</h4>
+                    These Terms apply to your use of the website/App. You must read these Terms carefully, and we recommend that you print and keep a copy for 
+                    your future reference. By accessing, browsing, using or registering for our Services on the website/App, you confirm that you have read, understood 
+                    and agree to these Terms in their entirety. If you do not agree to all of these Terms, you must not use this website/App or any Services related thereto.
+                    
+                    <br/><br/>
+
+                    <h4>Website/App Registration</h4>
+                    As an unregistered user you will be able to browse the Services offered on the website/App. However, when you choose to make use of the services 
+                    offered on this website/App, you will be required to login with your username and password. If you do not have a username and password you will be 
+                    required to register on the website or your mobile device. Registration requires filling in a short form which will create login details. 
+                    Once the registration is complete and you are logged in you will then be able to make use of the Services.
+                    The information you provide must be accurate and complete. You may not impersonate anyone else, choose names that are offensive or that violate 
+                    anyone’s rights. If you don’t follow these rules, we may cancel your account. Every person must register their own account and you are not 
+                    entitled to permit anyone other than you to use your account. Charges that are incurred in relation to the Services must be paid by way 
+                    of credit card or any other payment means as offered during the registration process. Your credit card details will be required.
+                    Purchases made via credit card will be verified, authenticated and stored by PayGate. PayGate is a certified Payment System Operator 
+                    (PASA) & PCI DSS Level 1 Service Provider, ensuring payment data security utilising PCI compliant processes and technology. 
+                    By registering an account or downloading the App, you expressly consent to ParkUpp collecting the personal information you provide, and to 
+                    ParkUpp processing your personal information as required to provide the Services. This will be done in compliance with ParkUpp’s Privacy Policy. 
+                    By registering on the website/App, you also expressly consent to receive direct marketing from ParkUpp about goods or services offered by ParkUpp 
+                    and any other affiliate, or entity within the ParkUpp group of companies.
+
+                    <br/><br/>
+
+                    <h4>Password / Account Security</h4>
+                    You are responsible for maintaining the confidentiality of your password and account and any activities that occur under your account. We will assume that 
+                    anyone using the website/App or transacting through your account is you. ParkUpp shall not be liable to any person for any loss or damage which may arise as a 
+                    result of any failure by you to protect your password or account.
+                    You hereby indemnify ParkUpp from any loss, liability, cost or expense resulting directly or indirectly from any unauthorised person accessing your account, 
+                    and waive any right to take action against ParkUpp, its directors, employees or affiliates in respect of any loss, liability, cost or expense resulting 
+                    directly or indirectly from any unauthorised person accessing and/or using your account.
+                    If you find out that someone has used your account without your permission, you must report it immediately to: info@parkupp.co.za At any time should you 
+                    wish to ascertain whether your vehicle has been registered to an account you may do so by providing suitable proof of ownership of the vehicle by email 
+                    to info@parkupp.co.za.
+                    
+                    <br/><br/>
+
+                    <h4>Accuracy of Content</h4>
+                    ParkUpp has taken all reasonable steps to ensure that all data described or depicted on this website/App and any other information displayed on the website/App 
+                    is current, complete and accurate. However, content errors may occur. The information displayed on our website/App is provided without any guarantees, conditions 
+                    or warranty as to its accuracy or completeness.
+                    To the extent permitted by applicable law, ParkUpp disclaims all representations and warranties, express or implied, that content or information displayed in or 
+                    on the website/App is accurate, complete, up-to- date and/or does not infringe the rights of any third party. Data provided by or through ParkUpp 
+                    and/or the website/App is provided by users or third party services providers, and is not verified or audited by ParkUpp. 
+                    The use of any content is forbidden in those places where the law does not allow this disclaimer to take full effect.
+                    
+                    <br/><br/>
+                    
+                    <h4>Our Rights</h4>
+                    We reserve the right to:
+                    <ul>
+                        <li>
+                            modify or withdraw, temporarily or permanently, the website/App (or any part of) with or without notice to you and you confirm that we shall not 
+                            be liable to you or any third party for any modification to or withdrawal of the website/App; and/or
+                        </li>
+                        <li>
+                            change these Terms from time to time, and your continued use of the website/App (or any part of) following such change shall be deemed
+                            to be your acceptance of such change. It is your responsibility to check regularly to determine whether the Terms have been changed. 
+                            If you do not agree to any change to the Terms then you must immediately stop using the website/App; and/or
+                        </li>
+                        <li>
+                            change the Services from time to time. We will use our reasonable endeavours to maintain the website/App. 
+                            You will not be eligible for any compensation because you cannot use any part of the website/App or because of a failure, 
+                            suspension or withdrawal of all or part of the website/App due to circumstances beyond our control
+                        </li>
+                    </ul>
 
 
                 </div>
