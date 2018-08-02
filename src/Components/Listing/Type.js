a05f74b7c030e4d250ddd53740fdc41f0d91db89
LeCray <mistercryptogrid@gmail.com>
2018-08-02 05:38:08 +0200

    Got the first part of stepzilla working

--- src/Components/Listing/Type.js
+++ src/Components/Listing/Type.js
@@ -55,101 +55,125 @@ export default class Type extends Component {
 
     render() {   
     return(  
-        <div>
-                                     
-        <div class="borderr container" style={{height: 500, padding: 40}}>                    
-            <h6 style={{marginBottom: 20}}>Please tell us the type of your parking spot</h6>              
+        
+        
+                               
+        <div class="container" style={{height: this.state.commercial?525:400, padding: 40, borderBottom: "1px solid white"}}>  
+            <h3 style={{marginBottom: 25}}>Please tell us the type of your parking spot</h3>
             
-            <div style={{marginBottom: 25}}>
+            <hr style={{borderColor: "#fff", marginBottom: 25}}/>
+            {/*TYPE OF PARKING*/}                  
+            <div >
+                <h4 class="pull-left" style={{display: "inline-block"}}>Type of Parking</h4>  
                 <select
-                    style={{width: 300, display: "inline-block"}}                
-                    className="form-control pull-left"
+                    style={{marginLeft: 20, width: 300, display: "inline-block"}}                
+                    className="form-control pull-right"
                     required
                     value={this.state.value} 
                     onChange={this.handleChange}
                     name="type">                    
-                        <option value="" disabled>Type of parking</option>
+                        <option value="" disabled>{this.state.type?this.state.type:"Type of parking"}</option>
                         <option value="Residential">Residential</option>
                         <option value="Commercial">Commercial</option>                      
                 </select>
-                <h4 style={{display: "inline-block"}}>{this.state.type}</h4>  
             </div>
             
-            <div style={{marginBottom: 25}}>
-                <div class="pull-left" style={{display: "inline-block"}}>
+            {/*TYPE OF PARKING == RESIDENTIAL*/}
+            <div style={{display: this.state.residential?"":"none"}}>
+                <h5 class="pull-left" style={{marginTop: 35, display: "inline-block", fontStyle: "italic"}}>
+                    What kind?
+                </h5>    
+                <div class="pull-right" style={{marginTop: 30, display: "inline-block"}}>
                     <select
-                        style={{width: 300, display: this.state.residential?"":"none"}}                
+                        style={{width: 300}}                
                         className="form-control"
                         required
                         value={this.state.value} 
                         onChange={this.handleChange}
                         name="resKind">                    
-                            <option value="" disabled>What kind?</option>
+                            <option value="" disabled>{this.state.resKind?this.state.resKind:"What kind?"}</option>
                             <option value="Driveway">Driveway</option>
                             <option value="ParkingLot">Parking Lot</option>                      
                     </select>
                 </div>
-                <h4 style={{display: "inline-block"}}>{this.state.Driveway?this.state.Drieway:this.state.ParkingLot}</h4>      
             </div>
             
-            <div >
+            {/*TYPE OF PARKING == COMMERCIAL*/}
+            <div style={{display: this.state.commercial?"":"none"}}>
+                <h5 class="pull-left" style={{marginTop: 35, display: "inline-block", fontStyle: "italic"}}>
+                    Number of parking bays
+                </h5>
                 <input
-                    style={{width: 300, display: this.state.commercial?"":"none"}}
+                    style={{width: 300, marginTop: 30, display: "inline-block"}}
+                    className="form-control pull-right"
                     name="numberOfBays"
                     autoComplete="off"
                     type="number"
-                    placeholder="Number of Parking bays"
-                    className="form-control"
+                    placeholder="Number of parking bays"
                     required
                     onChange={this.handleChange}/>
+
+                <h5 class="pull-left" style={{marginTop: 35, display: "inline-block", fontStyle: "italic"}}>
+                    Number of entrances
+                </h5>
                 <input
-                    style={{marginTop: 25, width: 300, display: this.state.commercial?"":"none"}}
+                    style={{width: 300, marginTop: 30, display: "inline-block"}}
+                    className="form-control pull-right"
                     name="numberOfEntrances"
                     autoComplete="off"
                     type="number"
-                    placeholder="Number of Entrances"
-                    className="form-control"
+                    placeholder="Number of Entrances"                    
                     required
                     onChange={this.handleChange}/>
+
+                <h5 class="pull-left" style={{marginTop: 35, display: "inline-block", fontStyle: "italic"}}>
+                    Do you have LPR?
+                </h5>
                 <select
-                    style={{marginTop: 24, width: 300, display: this.state.commercial?"":"none"}}                
-                    className="form-control"
+                    style={{width: 300, marginTop: 30, display: "inline-block"}}
+                    className="form-control pull-right"
                     required
                     value={this.state.value} 
                     onChange={this.handleChange}
                     name="lpr">                    
-                        <option value="" disabled>Do you have LPR installed?</option>
+                        <option value="" disabled>LPR?</option>
                         <option value="resDriveway">Yes</option>
                         <option value="resParkingLot">No</option>                      
                 </select>
             </div>
            
+            {/*IS THE SPACE LOCKED*/}
+            <div style={{}}>
+                <h4 class="pull-left" style={{marginTop: 30, display: "inline-block"}}>Is the space locked?</h4>
+                <select
+                    style={{marginTop: 30, display: "inline-block", width: 300}}
+                    name="locked"                
+                    className="form-control pull-right"
+                    required
+                    value={this.state.value} 
+                    onChange={this.handleChange}>                    
+                        <option value="">Is the space locked?</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>                      
+                </select>
+            </div>
 
-            <select
-                style={{marginTop: 0}}
-                name="locked"                
-                className="form-control"
-                required
-                value={this.state.value} 
-                onChange={this.handleChange}>                    
-                    <option value="">Is the space locked?</option>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>                      
-            </select>
-
-             <input
-                style={{marginTop: 25}}
-                name="numberOfBays"
-                autoComplete="off"
-                type="number"
-                placeholder="Number of bays"
-                className="form-control"
-                required
-                onChange={this.handleChange}/>
+            <div style={{}}>
+                <h4 class="pull-left" style={{marginTop: 30, display: "inline-block"}}>Number of bays</h4>
+                <input
+                    style={{marginTop: 30, width: 300, display: "inline-block"}}
+                    className="form-control pull-right"
+                    name="numberOfBays"
+                    autoComplete="off"
+                    type="number"
+                    placeholder="Number of bays"
+                    required
+                    onChange={this.handleChange}/>
+            </div>
                                               
-          </div>   
+        </div>   
                                                       
-        </div>
+        
         
     )}
 }
